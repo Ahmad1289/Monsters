a2cfd3c793be6ea16ac050844047bf406ebbb009
Ahmad1289 <51678366+Ahmad1289@users.noreply.github.com>
2019-07-24 18:37:28 +0300

    Update start.lua

--- start.lua
+++ start.lua
@@ -7,10 +7,10 @@ monsters_on = {string.match(monsters_dev:read('*all'), "^(.*)/(%d+)")}
 local monsters_file = io.open("sudo.lua", 'w')
 monsters_file:write("token = '" ..monsters_on[1].."'\n\nsudo_add = "..monsters_on[2].."" )
 monsters_file:close()
-https.request("https://api.telegram.org/bot"..monsters_on[1].."/sendMessage?chat_id="..monsters_on[2].."&text=Bot_monsters_is_start_new")
+https.request("https://api.telegram.org/bot"..monsters_on[1].."/sendMessage?chat_id="..monsters_on[2].."&text=Bot_Monsters_is_start_new")
 os.execute('cd .. && rm -rf .telegram-cli')
 os.execute('rm -rf monsters_online.lua')  
-os.execute('./tg -s ./Monsters.lua $@ --bot='..monsters_on[1])
+os.execute('./tg -s ./monsters.lua $@ --bot='..monsters_on[1])
 end
 function chack(tokenCk)
 local getme = "https://api.telegram.org/bot" ..tokenCk.. '/getme'
@@ -20,8 +20,8 @@ if data.ok == true then
 print("\27[31m✓ DONE\27[m \27[1;34m»»Now Send Sudo ID««\27[m")
 local sudo_send  = io.read()
 print("\27[31m✓ DONE\27[m")
-local tsheke_file = io.open("sudo.lua", 'w')
-monsters_file:write("token = '" ..tokenCk.."'\n\nsudo_add = "..sudo_send.."" )
+local monsters_file = io.open("sudo.lua", 'w')
+tsheke_file:write("token = '" ..tokenCk.."'\n\nsudo_add = "..sudo_send.."" )
 monsters_file:close()
 os.execute('cd .. && rm -fr .telegram-cli')
 os.execute('cd && rm -fr .telegram-cli')
