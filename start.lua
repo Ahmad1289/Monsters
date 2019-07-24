0bed0e0fb5f7ce52616247ddc255e2d7995f63c8
Ahmad1289 <51678366+Ahmad1289@users.noreply.github.com>
2019-07-24 16:00:05 +0300

    Update start.lua

--- start.lua
+++ start.lua
@@ -1,16 +1,16 @@
 sudo_monsters = dofile("sudo.lua")
 https = require("ssl.https")
 JSON = dofile("./libs/JSON.lua")
-local monsters_dev = io.open("monsters_online.lua")
-if monsters_dev then
-monsters_on = {string.match(monsters_dev:read('*all'), "^(.*)/(%d+)")}
-local monsters_file = io.open("sudo.lua", 'w')
-monsters_file:write("token = '" ..monsters_on[1].."'\n\nsudo_add = "..monsters_on[2].."" )
-monsters_file:close()
-https.request("https://api.telegram.org/bot"..monsters_on[1].."/sendMessage?chat_id="..monsters_on[2].."&text=Bot_monsters_is_start_new")
+local tshake_dev = io.open("monsters_online.lua")
+if tshake_dev then
+tshake_on = {string.match(tshake_dev:read('*all'), "^(.*)/(%d+)")}
+local tsheke_file = io.open("sudo.lua", 'w')
+tsheke_file:write("token = '" ..tshake_on[1].."'\n\nsudo_add = "..tshake_on[2].."" )
+tsheke_file:close()
+https.request("https://api.telegram.org/bot"..tshake_on[1].."/sendMessage?chat_id="..tshake_on[2].."&text=Bot_Monsters_is_start_new")
 os.execute('cd .. && rm -rf .telegram-cli')
 os.execute('rm -rf monsters_online.lua')  
-os.execute('./tg -s ./monsters.lua $@ --bot='..monsters_on[1])
+os.execute('./tg -s ./Monsters.lua $@ --bot='..tshake_on[1])
 end
 function chack(tokenCk)
 local getme = "https://api.telegram.org/bot" ..tokenCk.. '/getme'
@@ -20,12 +20,12 @@ if data.ok == true then
 print("\27[31m✓ DONE\27[m \27[1;34m»»Now Send Sudo ID««\27[m")
 local sudo_send  = io.read()
 print("\27[31m✓ DONE\27[m")
-local monsters_file = io.open("sudo.lua", 'w')
+local tsheke_file = io.open("sudo.lua", 'w')
 tsheke_file:write("token = '" ..tokenCk.."'\n\nsudo_add = "..sudo_send.."" )
-monsters_file:close()
+tsheke_file:close()
 os.execute('cd .. && rm -fr .telegram-cli')
 os.execute('cd && rm -fr .telegram-cli')
-os.execute('./tg -s ./monsters.lua $@ --bot='..tokenCk)
+os.execute('./tg -s ./Monsters.lua $@ --bot='..tokenCk)
 else
 print("\27[31m»»This TOKEN Incorrect , Send Right TOKEN««\27[m")
 local token_send = io.read()
@@ -40,12 +40,12 @@ chack(token_send)
 else 
 os.execute('cd .. && rm -fr .telegram-cli')
 os.execute('cd && rm -fr .telegram-cli')
-sudo_monsters = dofile("sudo.lua")
+sudo_tshake = dofile("sudo.lua")
 local getme = "https://api.telegram.org/bot" ..token.. '/getme'
 local req = https.request(getme)
 local data = JSON:decode(req)
 if data.ok == true then
-os.execute('./tg -s ./monsters.lua $@ --bot='..token)
+os.execute('./tg -s ./Monsters.lua $@ --bot='..token)
 else
 print("\27[31mTOKEN Incorrect , Send Right TOKEN««\27[m")
 local token_send = io.read()
