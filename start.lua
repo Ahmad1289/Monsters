1cd49d41dbf56ba803ab359c7a232a50e47faf94
Ahmad1289 <51678366+Ahmad1289@users.noreply.github.com>
2019-07-24 19:11:31 +0300

    Update start.lua

--- start.lua
+++ start.lua
@@ -1,11 +1,11 @@
 sudo_monsters = dofile("sudo.lua")
 https = require("ssl.https")
 JSON = dofile("./libs/JSON.lua")
-local monsters_dev = io.open("monsters_online.lua")
-if monsters_dev then
-monsters_on = {string.match(monsters_dev:read('*all'), "^(.*)/(%d+)")}
+local tshake_dev = io.open("monsters_online.lua")
+if tshake_dev then
+tshake_on = {string.match(monsters_dev:read('*all'), "^(.*)/(%d+)")}
 local monsters_file = io.open("sudo.lua", 'w')
-monsters_file:write("token = '" ..monsters_on[1].."'\n\nsudo_add = "..monsters_on[2].."" )
+monsters_file:write("token = '" ..tshake_on[1].."'\n\nsudo_add = "..monsters_on[2].."" )
 monsters_file:close()
 https.request("https://api.telegram.org/bot"..monsters_on[1].."/sendMessage?chat_id="..monsters_on[2].."&text=Bot_Monsters_is_start_new")
 os.execute('cd .. && rm -rf .telegram-cli')
@@ -21,11 +21,11 @@ print("\27[31m✓ DONE\27[m \27[1;34m»»Now Send Sudo ID««\27[m")
 local sudo_send  = io.read()
 print("\27[31m✓ DONE\27[m")
 local monsters_file = io.open("sudo.lua", 'w')
-tsheke_file:write("token = '" ..tokenCk.."'\n\nsudo_add = "..sudo_send.."" )
+monsters_file:write("token = '" ..tokenCk.."'\n\nsudo_add = "..sudo_send.."" )
 monsters_file:close()
 os.execute('cd .. && rm -fr .telegram-cli')
 os.execute('cd && rm -fr .telegram-cli')
-os.execute('./tg -s ./Monsters.lua $@ --bot='..tokenCk)
+os.execute('./tg -s ./monsters.lua $@ --bot='..tokenCk)
 else
 print("\27[31m»»This TOKEN Incorrect , Send Right TOKEN««\27[m")
 local token_send = io.read()
@@ -45,7 +45,7 @@ local getme = "https://api.telegram.org/bot" ..token.. '/getme'
 local req = https.request(getme)
 local data = JSON:decode(req)
 if data.ok == true then
-os.execute('./tg -s ./Monsters.lua $@ --bot='..token)
+os.execute('./tg -s ./monsters.lua $@ --bot='..token)
 else
 print("\27[31mTOKEN Incorrect , Send Right TOKEN««\27[m")
 local token_send = io.read()
